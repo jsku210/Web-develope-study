23714d4d772014dfe65e0336e9a264f94653c628
jsku210 <47470242+jsku210@users.noreply.github.com>
2023-03-27 17:41:02 +0900

    final class,method, Extends, Polymorphismm, Spring Legacy / java파일로 스프링관련 xml변환하기
    java : final class,method, Extends, Polymorphismm
    spring : java파일로 스프링관련 xml변환하기

---
+++ 공부내용/20230327/chapter6 example20/BankApplication.java
@@ -0,0 +1,114 @@
+import java.util.Scanner;
+
+public class BankApplication {
+	private static Account aabc = null;
+	private static int dd = 0;
+	private static Account[] accountArray = new Account[100];
+	private static Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		boolean run = true;
+		while (run) {
+			System.out.println("---------------------------------------------");
+			System.out.println("1.계좌생성 ㅣ 2.계좌목록 ㅣ 3.예금 ㅣ 4.출급 ㅣ 5.종료");
+			System.out.println("---------------------------------------------");
+			System.out.println("선택> ");
+
+			int selectNo = scanner.nextInt();
+
+			if (selectNo == 1) {
+				createAccount();
+			} else if (selectNo == 2) {
+				accountList();
+			} else if (selectNo == 3) {
+				deposit();
+			} else if (selectNo == 4) {
+				withdraw();
+			} else if (selectNo == 5) {
+				run = false;
+			}
+
+		}
+	}
+
+	private static void createAccount() {
+		// TODO Auto-generated method stub
+
+		System.out.println("계좌번호를 입력하세요");
+		String a = scanner.next();
+		System.out.println("이름을 입력하세요");
+		String b = scanner.next();
+		System.out.println("초기금액을 설정하세요");
+		int c = scanner.nextInt();
+
+		for (int i = 0; i <= accountArray.length; i++) {
+			if (accountArray[i] == null) {
+				Account aaa = new Account(a, b, c);
+				accountArray[i] = aaa;
+				accountArray[i].setAno(a);
+				accountArray[i].setOwner(b);
+				accountArray[i].setBalance(c);
+				System.out.println("생성되었습니다");
+				break;
+			} else {
+				continue;
+			}
+		}
+	}
+
+	private static void accountList() {
+		// TODO Auto-generated method stub
+		for (int i = 0; i <= accountArray.length; i++) {
+			if (accountArray[i] == null) {
+				break;
+			} else {
+				System.out
+						.println(accountArray[i].getAno() + accountArray[i].getOwner() + accountArray[i].getBalance());
+				continue;
+			}
+		}
+
+	}
+
+	private static void withdraw() { // 출금하기
+		// TODO Auto-generated method stub
+		System.out.println("출금하실려는 계좌번호를 입력하세요");
+		String a = scanner.next();
+		findAccount(a);
+		System.out.println("출금하실려는 금액을 입력하세요");
+		int b = scanner.nextInt();
+//		findAccount(a).setBalance(findAccount(a).getBalance() - b);
+		accountArray[dd].setBalance(accountArray[dd].getBalance() - b);
+	}
+
+	private static void deposit() { // 예금하기
+		// TODO Auto-generated method stub
+		System.out.println("예금하실려는 계좌번호를 입력하세요");
+		String a = scanner.next();
+		findAccount(a);
+		System.out.println("예금하실려는 금액을 입력하세요");
+		int b = scanner.nextInt();
+//		aabc.setBalance(findAccount(a).getBalance() + b);
+		accountArray[dd].setBalance(accountArray[dd].getBalance() + b);
+	}
+
+	private static void findAccount(String ano) {
+		for (int i = 0; i < accountArray.length; i++) {
+			if (accountArray[i] != null) {
+				if (ano == accountArray[i].getAno()) {
+					dd = i;
+//				String a = accountArray[i].getAno();
+//				String b = accountArray[i].getOwner();
+//				int c = accountArray[i].getBalance();
+//				aabc = new Account(a, b ,c);
+					break;
+				} else {
+					continue;
+				}
+			}
+		}
+//		return aabc;
+
+	}
+
+}
